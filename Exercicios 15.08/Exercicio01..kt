e70950fa7321c74a4f5f0563762693dfd945ce0b
Lucas Vasconcelos Lima <lucasv.lima@outlook.com>
2022-08-15 11:32:06 -0300

    Atualização de atividades local

---
+++ Exercicios 15.08/Exercicio01..kt
@@ -0,0 +1,14 @@
+fun main (){
+
+    /*
+    1 - O usuário digitará sua idade e o programa deverá printar o ano em que ele nasceu.
+
+    */
+
+    println("Digite sua idade: ")
+    var idade = readln().toInt()
+
+    println("Seu ano de nascimento foi: ${2022 - idade}")
+
+}
+
---
+++ Exercicios 15.08/Exercicio02..kt
@@ -0,0 +1,14 @@
+fun main () {
+
+    /*
+        2 - Faça um programa em que o usuário digite o diâmetro de um círculo
+        e calcule a área e o perímetro dele
+     */
+
+        println("Digite o diâmetro: ")
+        var diam = readln().toDouble()
+
+        println("A area do circulo é: ${(diam * diam) * 3.14}")
+
+
+}
