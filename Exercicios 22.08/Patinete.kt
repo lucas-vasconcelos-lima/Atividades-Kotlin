b8038cced5f43c1f5e4de43d130b87de8b5b79e6
Lucas Vasconcelos Lima <lucasv.lima@outlook.com>
2022-08-22 16:22:11 -0300

    correção Class

--- Exercicios 22.08/Patinete.kt
+++ Exercicios 22.08/Patinete.kt
@@ -1,7 +1,7 @@
         // Atributos - Cor, potencia, iluminação, rodas
         // Métodos - acelerar, mudar cor da iluminação, freiar
 
-class Patinete  {
+class Patinete {
 
     // Construtores secundários
     var ilumi = ""
@@ -10,21 +10,23 @@ class Patinete  {
     var rodas = ""
     var cor = ""
 
+
+    fun velocidade(acelerador: String) {
+        var velocidade = 0
+        println("A velocida atual é: $velocidade Km/h")
+
+        val acelerador = readln()
+        if (acelerador == "+") {
+            velocidade += 10
+        } else if (acelerador == "-" && velocidade >= 10) {
+            velocidade -= 10
+        } else if (acelerador == "-" && velocidade == 0) {
+            println("Você já está parado!")
+        } else {
+            println("Comando inválido!")
+        }
+        println("A velocida atual é: $velocidade Km/h")
+
+    }
+
 }
-fun velocidade(acelerador: String){
-    var velocidade = 0
-    println("A velocida atual é: $velocidade Km/h")
-
-    val acelerador = readln()
-    if(acelerador == "+"){
-        velocidade += 10
-    }else if(acelerador == "-" && velocidade >= 10){
-        velocidade -= 10
-    }else if(acelerador == "-" && velocidade == 0){
-        println("Você já está parado!")
-    }else{
-        println("Comando inválido!")
-    }
-    println("A velocida atual é: $velocidade Km/h")
-
-    }
